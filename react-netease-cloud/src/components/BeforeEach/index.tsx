e544c25a1a1ccf6e83c09c38d8d3b345b6379cbc
chenyxweb <chenyx_web@163.com>
2020-12-09 14:38:54 +0800

    函数组件实现页面跳转页面自动回到顶部功能组件

--- react-netease-cloud/src/components/BeforeEach/index.tsx
+++ react-netease-cloud/src/components/BeforeEach/index.tsx
@@ -1,24 +1,43 @@
 // 切换页面后回到顶部
 // 前置路由守卫
 
-import React from 'react'
+// 类组件写法
+// import React from 'react'
+// import { RouteComponentProps, withRouter } from 'react-router-dom'
+
+// interface IProps extends RouteComponentProps {}
+
+// class BeforeEach extends React.Component<IProps, {}> {
+//   componentDidUpdate(preProps: IProps) {
+//     if (this.props.location.pathname !== preProps.location.pathname) {
+//       // 滚动到顶部
+//       document.querySelector('#root > .app')?.scrollTo(0, 0)
+//     }
+//   }
+
+//   // state: IState = {}
+
+//   render() {
+//     return this.props.children
+//   }
+// }
+
+// export default withRouter(BeforeEach)
+
+// 函数组件写法
+import { FC, ReactElement, useEffect } from 'react'
 import { RouteComponentProps, withRouter } from 'react-router-dom'
 
 interface IProps extends RouteComponentProps {}
 
-class BeforeEach extends React.Component<IProps, {}> {
-  componentDidUpdate(preProps: IProps) {
-    if (this.props.location.pathname !== preProps.location.pathname) {
-      // 滚动到顶部
-      document.querySelector('#root > .app')?.scrollTo(0, 0)
-    }
-  }
-
-  // state: IState = {}
-
-  render() {
-    return this.props.children
-  }
+const BeforeEach: FC<IProps> = props => {
+  useEffect(() => {
+    // 滚动到顶部
+    document.querySelector('#root > .app')?.scrollTo(0, 0)
+  }, [props.location.pathname])
+  return props.children as ReactElement
 }
+
+BeforeEach.defaultProps = {}
 
 export default withRouter(BeforeEach)
